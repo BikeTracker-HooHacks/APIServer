19f59fa8d08c0271927f0cc1cb1fdfa0e062a835
William Zhang <willzhang05@gmail.com>
2021-03-27 17:23:05 -0400

    add base routers?

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 
 const express = require('express')
+const gpsRoutes = require('./gps')
+const userRoutes = require('./user')
+const trackerRoutes = require('./tracker')
+
 const app = express()
-const port = 3000
+const port = 8000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.use('/gps', gpsRoutes);
+
+app.use('/user', userRoutes);
+
+app.use('/tracker', trackerRoutes);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
