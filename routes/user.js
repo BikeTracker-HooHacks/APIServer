db85409e0a25356ff4ce4ba4f5ad6a22b9cb83c1
png <jasona99@outlook.com>
2021-03-28 01:15:43 -0400

    Untested user endpoints

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,29 @@
-const routes = require('express').Router();
+const router = require('express').Router();
 
-routes.get('/', (req, res) => {
+router.get('/', (req, res) => {
     res.status(200).json({ message: 'Connected to User endpoint!' });
 });
+
+router.post('/createUser', async (req, res) => {
+    username = req.body.username;
+    password = req.body.password;
+    if(username && password){
+        result = await db.createUser(username, password);
+        res.send(result);
+    }
+    else{
+        res.send("No username or password provided.");
+    }
+})
+
+router.post('/signin', async (req, res) => {
+    username = req.body.username;
+    password = req.body.password;
+    if(username && password){
+        result = await db.signIn(username, password, req)
+        res.send(result);
+    }
+
+})
 
 module.exports = routes;
