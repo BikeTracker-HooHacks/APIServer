670f24295e01314bd69c7eea934a9ececaba9d2a
png <jasona99@outlook.com>
2021-03-27 23:21:38 -0400

    Some db work and session setup

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-const express = require('express')
-const gpsRoutes = require('./routes/gps')
-const userRoutes = require('./routes/user')
-const trackerRoutes = require('./routes/tracker')
-require('dotenv').config()
+const express = require('express');
+const session = require('express-session');
+const gpsRoutes = require('./routes/gps');
+const userRoutes = require('./routes/user');
+const trackerRoutes = require('./routes/tracker');
+require('dotenv').config();
+const db = require('./db');
 
-const app = express()
-const port = process.env.PORT
+const app = express();
+const port = process.env.PORT;
+const sessionSecret = process.env.SESSIONSECRET;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 })
+
+app.use(session({
+  secret: sessionSecret,
+  resave: false,
+  saveUninitialized: false,
+  cookie: { sameSite: 'strict' },
+}));
 
 app.use('/gps', gpsRoutes);
 app.use('/user', userRoutes);
 app.use('/tracker', trackerRoutes);
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`)
+  console.log(`App listening at http://localhost:${port}`);
 })
